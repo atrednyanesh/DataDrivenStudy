48c5db8ae78761c4ad31ad3616b44ff623ef0583
cts1 <cts1>
2015-12-09 12:03:37 +0530

    dependancy resolved

---
+++ DataDriverDrameworkTestNG/src/main/java/com/framework/DataDriven/DataDriverDrameworkTestNG/test.java
@@ -0,0 +1,10 @@
+package com.framework.DataDriven.DataDriverDrameworkTestNG;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DataDriverDrameworkTestNG/src/test/java/com/framework/DataDriven/DataDriverDrameworkTestNG/TestRough.java
@@ -0,0 +1,17 @@
+package com.framework.DataDriven.DataDriverDrameworkTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class TestRough {
+	
+	public WebDriver driver;
+	
+	@Test
+	public void testing(){
+		
+	}
+	
+	
+
+}
